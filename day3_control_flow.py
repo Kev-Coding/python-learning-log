189efdcd6cbc092541759025ca050c6ded5323ed
Kev-Coding <mkh239@gmail.com>
2025-08-21 21:39:55 -0500

    completed day3_control_flow

--- day3_control_flow.py
+++ day3_control_flow.py
@@ -1,14 +1,32 @@
 # Todays lesson is if/elif/else
-is_pump_on = True
-if is_pump_on:
-    print("Pump is ON")
-else:
-    print("Pump is OFF")
+# is_pump_on = True
+# if is_pump_on:
+#     print("Pump is ON")
+# else:
+#     print("Pump is OFF")
 
-temperature = 33
+temperature = 99
+pressure = 33
+is_active = True
+
 if temperature > 100:
     print("Warning: Temperature exceeds safe limits!")
 elif temperature > 32:
     print("Temperature is within safe limits.")
 else:
     print("Warning: Temperature is below freezing.")
+
+
+# and/or/not f statements
+if pressure > 50 or temperature > 100:
+    print("Pump WARNING!")
+else:
+     print("Pump is normal.")
+
+# Nested if statements
+if is_active:
+    print("System is active.")
+    if temperature > 100:
+        print("Warning: High temperature detected!")
+    else:
+        print("Temperature is normal.")
